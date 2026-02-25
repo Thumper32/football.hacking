f073cc299a026803f7f6f6185b23908ae129adc5
Saulo Faria <saulo82faria@gmail.com>
2026-02-25 11:54:55 -0300

    Update football_main_app.py

--- football_main_app.py
+++ football_main_app.py
@@ -40,8 +40,6 @@ else:
         user['plan'] = user_on_db['plan']
     except Exception:
         user['plan'] = 'free'
-
-    st.user['plan'] = user['plan']
     
     pg_2 = st.navigation([
         st.Page('pages/home.py', title='Home'),
